5b204e9fb7e887781690473fe4acf8e0614da87c
Rodrigo Merino <ebedmail@gmail.com>
2016-08-01 20:44:56 -0400

    Se añade mejoras menu categorias y en nav ba
    Se ajusto un poco el layout del sitio y se agrego angular y una directiva para manejo de los acordeones

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Proyecto
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","angular-animate")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","stylesheets")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
