1fc307f746c3e59aa201bfa0bc63a1188bb45620
Rodrigo Merino <ebedmail@gmail.com>
2016-08-30 17:07:33 -0300

    Ajustes paperclip S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,12 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    :url => ":s3_domain_url",
+    :path => ":class/:id.:style.:extension",
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    :s3_host_name => "s3.amazonaws.com", # Added entry
-    :url => ":s3_host_name"                        # Added entry
+    }
   }
 end
