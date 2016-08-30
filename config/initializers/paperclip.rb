c026fdbca99c6a22e3662ba729278515894f671d
Rodrigo Merino <ebedmail@gmail.com>
2016-08-30 17:32:08 -0300

    Ajustes paperclip S3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 Paperclip::Attachment.default_options[:storage]        = :s3
-Paperclip::Attachment.default_options[:s3_region]      = 'oregon'
+
 Paperclip::Attachment.default_options[:s3_host_name]   = 's3.amazonaws.com',
 Paperclip::Attachment.default_options[:s3_credentials] = {
   bucket: ENV['S3_BUCKET_NAME'],
