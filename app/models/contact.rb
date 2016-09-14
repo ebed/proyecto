35a458788c4b871dc75aecdb035569f3b6e5488e
Rodrigo Merino <ebedmail@gmail.com>
2016-09-14 14:35:11 -0300

    Ajustes estilos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   belongs_to :tienda
+  belongs_to :user
   belongs_to :contactype
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910185845) do
+ActiveRecord::Schema.define(version: 20160914133416) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.float    "price",              limit: 24
