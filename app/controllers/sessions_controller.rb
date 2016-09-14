54c3aa4bd0b74558616c905a14473a115b956ab4
Rodrigo Merino <ebedmail@gmail.com>
2016-09-13 22:55:57 -0300

    Ajustes bootstrap y panel login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+# app/controllers/sessions_controller.rb
+class SessionsController < Devise::SessionsController
+
+ def new
+    super
+    # My thought here was just to call super
+    # and let Devise handle as normal w/o defining anything custom
+  end
+
+  def create
+    super
+  end
+
+  def destroy
+    super
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
 
 
+
+
   resources :statusorders
   resources :paymentmethods
   resources :profiles
