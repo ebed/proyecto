fb0aa8c0cabb373dbe3b000acef48c50a81149b7
Rodrigo Merino <ebedmail@gmail.com>
2016-11-12 01:47:03 -0300

    Correccion migracion

--- db/migrate/20161112005158_fix_relation_location_ubication.rb
+++ db/migrate/20161112005158_fix_relation_location_ubication.rb
@@ -1,10 +1,10 @@
 class FixRelationLocationUbication < ActiveRecord::Migration[5.0]
   def up
-  	remove_column :ubicacions, :location_id
+  	#remove_column :ubicacions, :location_id
 	add_column :locations, :ubication_id, :integer
   end
   def down
-	add_column :ubicacions, :location_id, :integer
+	#add_column :ubicacions, :location_id, :integer
 	remove_column :locations, :ubication_id
   end	
 end
