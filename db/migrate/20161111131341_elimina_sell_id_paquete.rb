9e4890a8cf7e0fb6bc0c399c4d1a2113bc196c7c
Rodrigo Merino <ebedmail@gmail.com>
2016-11-12 01:30:52 -0300

    Correccion migracion

--- db/migrate/20161111131341_elimina_sell_id_paquete.rb
+++ db/migrate/20161111131341_elimina_sell_id_paquete.rb
@@ -1,9 +1,9 @@
 class EliminaSellIdPaquete < ActiveRecord::Migration[5.0]
   def up
-  	remove_column :paquetes, :sell_id
+#  	remove_column :paquetes, :sell_id
   end
   
    def down
-  	add_column :paquetes, :sell_id, :integer
+ # 	add_column :paquetes, :sell_id, :integer
   end
 end
