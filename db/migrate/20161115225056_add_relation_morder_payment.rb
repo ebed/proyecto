a69688a8740aec3e1d8addd778e251f04de94c0f
Rodrigo Merino <ebedmail@gmail.com>
2016-11-21 10:40:45 -0300

    Ajustes a scripts de migracion para corregir inconsistencias solicitadas en EB para reparar

--- db/migrate/20161115225056_add_relation_morder_payment.rb
+++ db/migrate/20161115225056_add_relation_morder_payment.rb
@@ -1,13 +1,13 @@
 class AddRelationMorderPayment < ActiveRecord::Migration[5.0]
   def up
   	add_column :main_orders, :payment_id, :integer
-  	remove_column :main_orders, :total
+  #	remove_column :main_orders, :total
   	remove_column :sells, :payment_id
   	
   end
     def down
   	remove_column :main_orders, :payment_id
-  	add_column :main_orders, :total, :integer
+  	#add_column :main_orders, :total, :integer
   	add_column :sells, :payment_id, :integer
   	
   end
